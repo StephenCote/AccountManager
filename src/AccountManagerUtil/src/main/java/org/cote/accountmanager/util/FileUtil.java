e2716fe06282e2af9bae6f573ccd96130766181d
Stephen Cote <sw.cote@gmail.com>
2015-06-15 21:39:37 -0700

    AM Util Header Revision

--- src/AccountManagerUtil/src/main/java/org/cote/accountmanager/util/FileUtil.java
+++ src/AccountManagerUtil/src/main/java/org/cote/accountmanager/util/FileUtil.java
@@ -1,3 +1,26 @@
+/*******************************************************************************
+ * Copyright (C) 2002, 2015 Stephen Cote Enterprises, LLC. All rights reserved.
+ * Redistribution without modification is permitted provided the following conditions are met:
+ *
+ *    1. Redistribution may not deviate from the original distribution,
+ *        and must reproduce the above copyright notice, this list of conditions
+ *        and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *    2. Products may be derived from this software.
+ *    3. Redistributions of any form whatsoever must retain the following acknowledgment:
+ *        "This product includes software developed by Stephen Cote Enterprises, LLC"
+ *
+ * THIS SOFTWARE IS PROVIDED BY STEPHEN COTE ENTERPRISES, LLC ``AS IS''
+ * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THIS PROJECT OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 package org.cote.accountmanager.util;
 
 import java.io.ByteArrayOutputStream;
@@ -53,11 +76,15 @@ public class FileUtil {
 		}
 		return copied;
 	}
-	public static boolean emitFile(String path, String contents){
+	public static boolean emitFile(String path, String contents)
+	{
+		return emitFile(path, contents, "UTF-8");
+	}
+	public static boolean emitFile(String path, String contents, String encoding){
 		byte[] cont = new byte[0];
 		boolean out_bool = false;
 		try {
-			cont = contents.getBytes("UTF-8");
+			cont = contents.getBytes(encoding);
 			out_bool = emitFile(path, cont);
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
