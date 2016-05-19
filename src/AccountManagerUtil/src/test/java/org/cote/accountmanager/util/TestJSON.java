754f938662ad0f6f55dad4c342dc834d5903e6bc
Stephen Cote <sw.cote@gmail.com>
2016-05-18 22:50:29 -0700

    Add test for simplifying policy structure imports

--- src/AccountManagerUtil/src/test/java/org/cote/accountmanager/util/TestJSON.java
+++ src/AccountManagerUtil/src/test/java/org/cote/accountmanager/util/TestJSON.java
@@ -1,12 +1,14 @@
 package org.cote.accountmanager.util;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.security.Security;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.cote.accountmanager.objects.DataType;
+import org.cote.accountmanager.objects.PolicyType;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,6 +22,33 @@ public class TestJSON {
 			System.out.println("Properties=" + log4jPropertiesPath);
 			PropertyConfigurator.configure(log4jPropertiesPath);
 		}
+	}
+	/*
+		Actor {Can/Can't} {Do} PermissionSuffix Type
+		Rule
+	*/	
+	public String getImportType(){
+		return "{"
+			+ "\"name\":\"Test Name\""
+			+ ",\"rules\":["
+				+ "{"
+					+ "\"name\":\"Test Sub Type\""
+				+ "}"
+			+ "]"
+			+ "}"
+		;
+	}
+	
+	@Test
+	public void testSimplifiedTypeImport(){
+		PolicyType policy = JSONUtil.importObject(getImportType(), PolicyType.class);
+		assertNotNull("Imported type is null",policy);
+		assertTrue("Imported type missing child type",policy.getRules().size() > 0);
+	
+		policy = JSONUtil.importObject(FileUtil.getFileAsString("./testType.json"), PolicyType.class);
+		assertNotNull("Imported type #2 is null",policy);
+		
+		//FactEnumType.
 	}
 	
 	@Test
