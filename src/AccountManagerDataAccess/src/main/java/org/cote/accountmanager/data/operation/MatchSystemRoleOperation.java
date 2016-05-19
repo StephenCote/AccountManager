1763b39d6c002bac5b62c08015500d51a2a56ad5
Stephen Cote <sw.cote@gmail.com>
2016-05-18 22:55:01 -0700

    Operation to match system roles

---
+++ src/AccountManagerDataAccess/src/main/java/org/cote/accountmanager/data/operation/MatchSystemRoleOperation.java
@@ -0,0 +1,56 @@
+package org.cote.accountmanager.data.operation;
+
+import org.apache.log4j.Logger;
+import org.cote.accountmanager.data.ArgumentException;
+import org.cote.accountmanager.data.Factories;
+import org.cote.accountmanager.data.FactoryException;
+import org.cote.accountmanager.data.fact.FactUtil;
+import org.cote.accountmanager.objects.BaseRoleType;
+import org.cote.accountmanager.objects.FactType;
+import org.cote.accountmanager.objects.OperationResponseEnumType;
+import org.cote.accountmanager.objects.PatternType;
+import org.cote.accountmanager.objects.types.NameEnumType;
+import org.cote.accountmanager.objects.types.RoleEnumType;
+
+public class MatchSystemRoleOperation implements IOperation {
+	public static final Logger logger = Logger.getLogger(MatchSystemRoleOperation.class.getName());
+
+	public <T> T read(FactType sourceFact,final FactType referenceFact){
+		return FactUtil.factoryRead(sourceFact, referenceFact);
+	}
+	public OperationResponseEnumType operate(final PatternType pattern, FactType sourceFact,final FactType referenceFact){
+		if(sourceFact.getFactReference() == null || sourceFact.getFactReference().getNameType() == NameEnumType.UNKNOWN){
+			logger.error("Invalid argument");
+			return OperationResponseEnumType.ERROR;
+		}
+		if(!sourceFact.getFactReference().getNameType().equals(NameEnumType.valueOf(referenceFact.getFactType().toString()))){
+			logger.warn("Source type " + sourceFact.getFactReference().getNameType().toString() + " doesn't match " + referenceFact.getFactType().toString());
+			return OperationResponseEnumType.FAILED;
+		}
+		BaseRoleType systemRole = null;
+		try {
+			systemRole = Factories.getRoleFactory().getRoleByName(referenceFact.getSourceUrn(), null, ((BaseRoleType)sourceFact.getFactReference()).getRoleType(), referenceFact.getOrganizationId());
+				
+			
+		} catch (FactoryException | ArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(systemRole == null){
+			logger.error("Invalid " + ((BaseRoleType)sourceFact.getFactReference()).getRoleType().toString() + " role '" + referenceFact.getSourceUrn() + "' in #" + referenceFact.getOrganizationId());
+			return OperationResponseEnumType.FAILED;
+		}
+		if(systemRole.getId().equals(sourceFact.getFactReference().getId())){
+			return OperationResponseEnumType.SUCCEEDED;
+		}
+		logger.debug("Role " + systemRole.getUrn() + " does not match parameter role with id " + sourceFact.getFactReference().getUrn());
+		return OperationResponseEnumType.FAILED;
+		
+		//getAccountRole("DataAdministrators", null, organizationId);
+		//logger.debug("Supplied fact reference matches type " + referenceFact.getFactoryType().toString());
+		
+		
+	}
+
+}
