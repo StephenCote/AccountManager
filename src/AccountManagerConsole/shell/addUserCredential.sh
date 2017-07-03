bf245587a81ac7bdea754199634e42a350f189fa
Stephen Cote <sw.cote@gmail.com>
2017-07-03 15:12:33 -0700

    Migrate user credential reset from previous version - temporary
    Temporary migration of user credential reset.  This is still lacking as
    far as applying any kind of policy to the credential, etc.

---
+++ src/AccountManagerConsole/shell/addUserCredential.sh
@@ -0,0 +1,24 @@
+# org, username, targetUserName, newUserPassword
+parentObjName=$(./hash.sh "$1.USER.$3")
+userCacheName="cache/object.$parentObjName.json"
+
+res=$(./getUser.sh "$1" "$2" "$3")
+if [ -f $userCacheName ]; then
+   userObjectId=$(./extractObjectId.sh $userCacheName) 
+   echo "Composing add $1/$3 organization request in $1 (id: $userObjectId)..."
+   # create the new credential stub
+   res=$(./genPwdAuth.sh "$1" "$3" "$4")
+
+   authName=$(./hash.sh "$1.$2")
+   newAuthName=$(./hash.sh "$1.$3")
+   url=$(./encode.sh "http://127.0.0.1:8080/AccountManagerService/rest/token/credential/USER/$userObjectId")
+   curl -sS -X "POST" -H "Content-Type: application/json" -H "Authorization: Bearer $(cat cache/auth.$authName.token)" -d @cache/auth.$newAuthName.json $url
+   echo ""
+   echo "Note: Previous credentials are demoted from being primary, but persist until they are removed or expired"
+   echo "Requesting new token for $3"
+   res=$(./refreshToken.sh "$1" "$3" "$4")
+   echo "Token: $res"
+   echo ""
+else
+   echo "User $3 not found in $1."
+fi
--- src/AccountManagerService/src/main/java/org/cote/rest/services/TokenService.java
+++ src/AccountManagerService/src/main/java/org/cote/rest/services/TokenService.java
@@ -1,9 +1,11 @@
 package org.cote.rest.services;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -20,15 +22,29 @@ import org.cote.accountmanager.beans.SecurityBean;
 import org.cote.accountmanager.data.ArgumentException;
 import org.cote.accountmanager.data.Factories;
 import org.cote.accountmanager.data.FactoryException;
+import org.cote.accountmanager.data.factory.AccountFactory;
+import org.cote.accountmanager.data.factory.DataFactory;
+import org.cote.accountmanager.data.factory.GroupFactory;
 import org.cote.accountmanager.data.factory.OrganizationFactory;
+import org.cote.accountmanager.data.security.CredentialService;
+import org.cote.accountmanager.data.services.AuditService;
+import org.cote.accountmanager.data.services.RoleService;
 import org.cote.accountmanager.data.services.SessionSecurity;
+import org.cote.accountmanager.objects.AuditType;
 import org.cote.accountmanager.objects.AuthenticationRequestType;
 import org.cote.accountmanager.objects.AuthenticationResponseEnumType;
 import org.cote.accountmanager.objects.AuthenticationResponseType;
+import org.cote.accountmanager.objects.BaseGroupType;
 import org.cote.accountmanager.objects.CredentialEnumType;
+import org.cote.accountmanager.objects.CredentialType;
+import org.cote.accountmanager.objects.NameIdType;
 import org.cote.accountmanager.objects.OrganizationType;
 import org.cote.accountmanager.objects.UserType;
+import org.cote.accountmanager.objects.types.ActionEnumType;
+import org.cote.accountmanager.objects.types.AuditEnumType;
 import org.cote.accountmanager.objects.types.FactoryEnumType;
+import org.cote.accountmanager.objects.types.NameEnumType;
+import org.cote.accountmanager.service.rest.BaseService;
 import org.cote.accountmanager.service.rest.SchemaBean;
 import org.cote.accountmanager.service.rest.ServiceSchemaBuilder;
 import org.cote.accountmanager.service.util.ServiceUtil;
@@ -53,6 +69,111 @@ public class TokenService {
 		 return schemaBean;
 	 }
 
+	@POST @Path("/credential/{type:[A-Za-z]+}/{objectId:[A-Za-z0-9\\-\\.]+}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public boolean newPrimaryCredential(@PathParam("type") String objectType, @PathParam("objectId") String objectId,AuthenticationRequestType authReq,@Context HttpServletRequest request){
+		AuditType audit = AuditService.beginAudit(ActionEnumType.MODIFY, "newCredential", AuditEnumType.SESSION, ServiceUtil.getSessionId(request));
+		AuditEnumType type = AuditEnumType.valueOf(objectType);
+		UserType user = ServiceUtil.getUserFromSession(audit,request);
+		UserType owner = null;
+		boolean out_bool = false;
+		if(user == null) return out_bool;
+		CredentialType newCred = null;
+		try{
+			NameIdType targetObject = null;
+			
+			/*
+			 * To create a new user credential:
+			 *    The authenticated user must be an account administrator
+			 *    Or the current credential must be supplied
+			 */
+			//boolean accountAdmin = org.cote.accountmanager.data.services.AuthorizationService.is
+			boolean accountAdmin = RoleService.isFactoryAdministrator(user,((AccountFactory)Factories.getFactory(FactoryEnumType.ACCOUNT)));
+			boolean dataAdmin = RoleService.isFactoryAdministrator(user, ((DataFactory)Factories.getFactory(FactoryEnumType.DATA)));
+			if(type == AuditEnumType.USER){
+				UserType updateUser = BaseService.readByObjectId(AuditEnumType.valueOf(objectType), objectId, request);
+				//UserType updateUser = Factories.getNameIdFactory(FactoryEnumType.USER).getByName(authReq.getSubject(), user.getOrganizationId());
+				if(updateUser == null){
+					AuditService.denyResult(audit, "Target user " + authReq.getSubject() + " does not exist");
+					return out_bool;
+				}
+				AuditService.targetAudit(audit, AuditEnumType.USER, updateUser.getName() + " (#" + updateUser.getId() + ")");
+				/// If not account admin, then validate the current password
+				if(accountAdmin == false){
+					if(org.cote.accountmanager.data.services.AuthorizationService.isMapOwner(user, updateUser) == false){
+						AuditService.denyResult(audit, "Non account administrators are not premitted to change user passowrds that are not their own.");
+						return out_bool;
+					}
+					CredentialType currentCred = CredentialService.getPrimaryCredential(updateUser);
+					if(authReq.getCheckCredential() == null || authReq.getCheckCredential().length == 0){
+						AuditService.denyResult(audit, "The current credential is required to create a new one.");
+						return out_bool;
+					}
+					if(CredentialService.validatePasswordCredential(updateUser, currentCred, new String(authReq.getCheckCredential(),"UTF-8"))==false){
+						AuditService.denyResult(audit, "Failed to validate current credential");
+						return out_bool;
+					}
+					
+				}
+				targetObject = updateUser;
+				owner = updateUser;
+			}
+			/*
+			 * To create an object credential,
+			 * the current user must be a data administrator
+			 * Or the object owner
+			 * Or have access to update the object and supply current password must be supplied
+			 */
+			else if(type == AuditEnumType.GROUP){
+				BaseGroupType updateGroup = ((GroupFactory)Factories.getFactory(FactoryEnumType.GROUP)).getByUrn(authReq.getSubject());
+				if(updateGroup == null){
+					AuditService.denyResult(audit, "Target group " + authReq.getSubject() + " does not exist");
+					return out_bool;
+				}
+				AuditService.targetAudit(audit, AuditEnumType.GROUP, updateGroup.getName() + " (#" + updateGroup.getId() + ")");
+				/// If not account admin, then validate the current password
+				if(dataAdmin == false && org.cote.accountmanager.data.services.AuthorizationService.canChange(user, updateGroup) == false){
+
+					CredentialType currentCred = CredentialService.getPrimaryCredential(updateGroup);
+					if(authReq.getCheckCredential() == null || authReq.getCheckCredential().length == 0){
+						AuditService.denyResult(audit, "The current credential is required to create a new one.");
+						return out_bool;
+					}
+					if(CredentialService.validatePasswordCredential(updateGroup, currentCred, new String(authReq.getCheckCredential(),"UTF-8"))==false){
+						AuditService.denyResult(audit, "Failed to validate current credential");
+						return out_bool;
+					}
+				}
+				targetObject = updateGroup;
+				owner = Factories.getNameIdFactory(FactoryEnumType.USER).getById(updateGroup.getOwnerId(), updateGroup.getOrganizationId());
+			}
+			else{
+				AuditService.denyResult(audit, "Subject type " + authReq.getSubjectType().toString() + " is unsupported");;
+				return out_bool;
+			}
+			/// Create a new primary credential for target user
+			byte[] credByte = (new String(authReq.getCredential())).trim().getBytes("UTF-8");
+			newCred =
+					//CredentialService.newHashedPasswordCredential(owner, targetObject, new String(authReq.getCredential(),"UTF-8"), true, false);
+					
+					CredentialService.newCredential(authReq.getCredentialType(), null, owner, targetObject, credByte, true, true, false);
+		}
+		catch(FactoryException | ArgumentException | UnsupportedEncodingException e) {
+			logger.error("Error",e);
+		} 
+		if(newCred != null){
+			AuditService.permitResult(audit, "Created a new primary credential");
+			out_bool = true;
+		}
+		else{
+			AuditService.denyResult(audit, "Failed to create new primary credential");
+		}
+		return out_bool;
+
+	}
+
+	
 	@POST
 	@Path("/jwt/authenticate")
 	@Produces(MediaType.APPLICATION_JSON)
