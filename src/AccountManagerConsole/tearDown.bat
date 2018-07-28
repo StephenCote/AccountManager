7a297aaaac66abd729d60db214f86fc332645e6c
Stephen Cote <sw.cote@gmail.com>
2018-07-28 12:50:01 -0700

    Updates for local environment tests

--- src/AccountManagerConsole/tearDown.bat
+++ src/AccountManagerConsole/tearDown.bat
@@ -2,7 +2,7 @@
 cd target
 echo ***** Configuring AM6 and Rocket Schema *****
 REM call java -jar AccountManagerConsole.jar -setup -rootPassword password -schema "C:\Users\Stephen Cote\Documents\GitHub\AccountManager\db\postgres\AM6_PG9_Schema.sql" -confirm
-call java -jar AccountManagerConsole.jar -setup -rootPassword password -schema "C:\Users\Stephen Cote\Documents\GitHub\AccountManager\db\postgres\AM6_PG9_Schema.sql" -rocketSchema "C:\Users\Stephen Cote\Documents\GitHub\AccountManager\db\postgres\Rocket_PG9_Schema.sql" -confirm
+call java -jar AccountManagerConsole.jar -setup -rootPassword password -schema "C:\Users\swcot\Documents\GitHub\AccountManager\db\postgres\AM6_PG9_Schema.sql" -rocketSchema "C:\Users\swcot\Documents\GitHub\AccountManager\db\postgres\Rocket_PG9_Schema.sql" -confirm
 echo ***** Adding FirstContact organization *****
 echo NOTE: Organizations added at the root can only be added via the AccountManagerConsole.  This is by design.
 call java -jar AccountManagerConsole.jar -addOrganization -organization / -name FirstContact -adminPassword password -password password
---
+++ src/AccountManagerObjects/src/lib/installToMaven.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=jaxb2-primitive-fixer-plugin-1.0.0-SNAPSHOT.jar -DgroupId=de.sweber -DartifactId=jaxb2-primitive-fixer-plugin -Dversion=1.0.0-SNAPSHOT -Dpackaging=jar -DlocalRepository=c:\users\swcot\.m2
