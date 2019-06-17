c9b0c6e056dc0feea97163c753d3e26782e32571
Stephen Cote <sw.cote@gmail.com>
2019-06-17 16:49:25 -0700

    Unit test community project creation
    Running into an issue where creating a community project from a browser versus a script (both using the same API endpoint) is resulting in an object reference error for supplemental project assets (I.E.: Time and Cost values).  Updated unit test to investigate.

--- src/Accelerant/src/test/java/org/cote/rocket/test/TestCommunities.java
+++ src/Accelerant/src/test/java/org/cote/rocket/test/TestCommunities.java
@@ -7,16 +7,23 @@ import java.util.UUID;
 
 import org.cote.accountmanager.data.ArgumentException;
 import org.cote.accountmanager.data.Factories;
+import org.cote.accountmanager.data.factory.GroupFactory;
 import org.cote.accountmanager.data.factory.UserFactory;
 import org.cote.accountmanager.data.services.ICommunityProvider;
+import org.cote.accountmanager.data.util.UrnUtil;
 import org.cote.accountmanager.exceptions.FactoryException;
 import org.cote.accountmanager.objects.DirectoryGroupType;
 import org.cote.accountmanager.objects.OrganizationType;
 import org.cote.accountmanager.objects.UserType;
 import org.cote.accountmanager.objects.types.AuditEnumType;
 import org.cote.accountmanager.objects.types.FactoryEnumType;
+import org.cote.accountmanager.objects.types.UserEnumType;
+import org.cote.accountmanager.objects.types.UserStatusEnumType;
 import org.cote.accountmanager.service.rest.BaseService;
+import org.cote.accountmanager.util.JSONUtil;
+import org.cote.propellant.objects.LifecycleType;
 import org.cote.propellant.objects.ProjectType;
+import org.cote.rocket.Rocket;
 import org.cote.rocket.util.CommunityProjectUtil;
 import org.junit.Test;
 
@@ -46,6 +53,8 @@ public class TestCommunities extends BaseAccelerantTest {
 	}
 	*/
 	
+	/*
+	
 	@Test
 	public void TestNewCommunityProject(){
 		String newProjectName = "Project-" + UUID.randomUUID().toString();
@@ -56,5 +65,118 @@ public class TestCommunities extends BaseAccelerantTest {
 		newProject.setGroupPath(pDir.getPath());
 		boolean saved = CommunityProjectUtil.saveCommunityProject(newProject, testUser);
 		assertTrue("Project was not saved", saved);
+	}
+	*/
+	
+	private UserType getCommunityUser(OrganizationType org, String name) {
+		UserType user = null;
+
+		try {
+			user = ((UserFactory)Factories.getFactory(FactoryEnumType.USER)).getByName(name, org.getId());
+			if(user == null) {
+				user = ((UserFactory)Factories.getNameIdFactory(FactoryEnumType.USER)).newUser(name, UserEnumType.NORMAL, UserStatusEnumType.NORMAL, org.getId());
+				if(Factories.getNameIdFactory(FactoryEnumType.USER).add(user) == false){
+					logger.error("Failed to create user");
+					return null;
+				}
+				user = Factories.getNameIdFactory(FactoryEnumType.USER).getByName(name, org.getId());
+
+			}
+		} catch (FactoryException | ArgumentException e) {
+			logger.error(e);
+		}
+		return user;
+	}
+	private UserType getCommunityAdminUser(OrganizationType org) {
+		UserType adminUser = null;
+		try {
+			adminUser = ((UserFactory)Factories.getFactory(FactoryEnumType.USER)).getByName("Admin", org.getId());
+		} catch (FactoryException | ArgumentException e) {
+			logger.error(e);
+		}
+		return adminUser;
+	}
+	private OrganizationType getCommunityTestOrganization(String name){
+		String newOrgName = "QA Org " + name;
+		logger.info("Creating new test organization: " + newOrgName);
+		OrganizationType org = getCreateOrganization(newOrgName,"password");
+		assertNotNull("New organization is null", org);
+		ICommunityProvider provider = getProvider();
+		UserType adminUser = getCommunityAdminUser(org);
+
+		assertNotNull("Admin user is null",adminUser);
+		
+		boolean configured = provider.isCommunityConfigured(org.getId());
+		if(!configured) {
+			logger.info("Community is not configured.  Attempting to configure");
+			configured = provider.configureCommunity(adminUser);
+		}
+		else {
+			logger.info("Organization is already configured for community use");
+		}
+		assertTrue("Failed to configure community", configured);
+		return org;
+	}
+	
+	@Test
+	public void TestNewCommunityLifecycleProject(){
+		OrganizationType testCommunityOrg = getCommunityTestOrganization("TestCommunities");
+		
+		String testId = UUID.randomUUID().toString();
+		String newProjectName = "Project-" + testId;
+		String newProjectName2 = "Project 2-" + testId;
+		String newLifecycleName = "Lifecycle-Q1";// + UUID.randomUUID().toString();
+		
+		ICommunityProvider provider = getProvider();
+		UserType adminUser = getCommunityAdminUser(testCommunityOrg);
+		UserType user = getCommunityUser(testCommunityOrg, "Test Project User 1");
+		
+		
+		boolean createCommunity = false;
+		boolean createProject = false;
+		boolean createProject2 = false;
+		try {
+				
+				LifecycleType lt = provider.getCommunity(adminUser, newLifecycleName);
+				if(lt == null) {
+					createCommunity = provider.createCommunity(adminUser, newLifecycleName);
+					assertTrue("Failed to create community", createCommunity);
+					lt = provider.getCommunity(adminUser, newLifecycleName);
+				}
+				assertNotNull("Failed to find community", lt);
+				
+				ProjectType pj = provider.getCommunityProject(adminUser, newLifecycleName, newProjectName);
+				if(pj == null) {
+					createProject = provider.createCommunityProject(adminUser, lt.getObjectId(), newProjectName);
+					assertTrue("Failed to create community project", createProject);
+					pj = provider.getCommunityProject(adminUser, newLifecycleName, newProjectName);
+				}
+				assertNotNull("Failed to find community project", lt);
+				
+				provider.enrollAdminInCommunity(adminUser, lt.getObjectId(), user.getObjectId());
+				
+				ProjectType pj2 = provider.getCommunityProject(adminUser, newLifecycleName, newProjectName2);
+				if(pj2 == null) {
+					createProject2 = provider.createCommunityProject(user, lt.getObjectId(), newProjectName2);
+					assertTrue("Failed to create community project", createProject2);
+					pj2 = provider.getCommunityProject(user, newLifecycleName, newProjectName2);
+				}
+				assertNotNull("Failed to find community project", lt);
+				
+		}
+		catch(Exception e) {
+			logger.error(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		/*
+		DirectoryGroupType pDir = BaseService.makeFind(AuditEnumType.GROUP, "DATA", "~/Projects", testUser);
+		assertNotNull("Directory is null", pDir);
+		ProjectType newProject = new ProjectType();
+		newProject.setName(newProjectName);
+		newProject.setGroupPath(pDir.getPath());
+		boolean saved = CommunityProjectUtil.saveCommunityProject(newProject, testUser);
+		assertTrue("Project was not saved", saved);
+		*/
 	}
 }
