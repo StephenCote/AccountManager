13148c8364743fddaf9bd833706a5cbec1f85524
Stephen Cote <sw.cote@gmail.com>
2019-11-26 16:45:46 -0800

    Add option to pass in an existing bulk session id when creating a user as a person

--- src/AccountManagerDataAccess/src/main/java/org/cote/accountmanager/data/services/PersonService.java
+++ src/AccountManagerDataAccess/src/main/java/org/cote/accountmanager/data/services/PersonService.java
@@ -64,12 +64,32 @@ public class PersonService {
 		return createUserAsPerson(audit, userName, password,email,UserEnumType.NORMAL,UserStatusEnumType.REGISTERED,organizationId);
 	}
 	public static boolean createUserAsPerson(AuditType audit, String userName, String password, String email,UserEnumType userType,UserStatusEnumType userStatus,long organizationId){
-		boolean outBool = false;
-		try{
+		boolean created = false;
+		try {
 			if(((UserFactory)Factories.getNameIdFactory(FactoryEnumType.USER)).getUserNameExists(userName, organizationId)){
 				logger.error("User name '" + userName + "' is already used in organization " + organizationId);
 				return false;
 			}
+			String sessionId = BulkFactories.getBulkFactory().newBulkSession();
+			
+			createUserAsPerson(audit, sessionId, userName, password, email, userType, userStatus, organizationId);
+			
+			BulkFactories.getBulkFactory().write(sessionId);
+			BulkFactories.getBulkFactory().close(sessionId);
+			
+			EffectiveAuthorizationService.rebuildPendingRoleCache();
+		}
+		catch(ArgumentException | FactoryException | DataAccessException e) {
+			
+			logger.error(e.getMessage());
+			logger.error(FactoryException.LOGICAL_EXCEPTION,e);
+		}
+		return created;
+	}
+	public static boolean createUserAsPerson(AuditType audit, String sessionId, String userName, String password, String email,UserEnumType userType,UserStatusEnumType userStatus,long organizationId){
+
+		boolean outBool = false;
+		try{
 
 			/// TODO - change this to just get the persons directory from the GroupFactory
 			///
@@ -78,16 +98,14 @@ public class PersonService {
 			DirectoryGroupType pDir = ((GroupFactory)Factories.getFactory(FactoryEnumType.GROUP)).getCreateDirectory(adminUser, "Persons", ((GroupFactory)Factories.getFactory(FactoryEnumType.GROUP)).getRootDirectory(organizationId), organizationId);
 			DirectoryGroupType cDir = ((GroupFactory)Factories.getFactory(FactoryEnumType.GROUP)).getCreateDirectory(adminUser, "Contacts", ((GroupFactory)Factories.getFactory(FactoryEnumType.GROUP)).getRootDirectory(organizationId), organizationId);
 			
-			String sessionId = BulkFactories.getBulkFactory().newBulkSession();
-			
 			/// The logic is inverted here because the design is/was to have users own objects (all ownerId values point to users)
 			/// And a user must own the person object, but because this person belongs to the user,
 			/// the user is created first, then the person is added, but the person becomes the primary point of entry
 			/// And this user is the primary owner.  Subsequent users attached to this person become dependent on the first, which owns the person
 			///
 			/// Since the Person structure is meant to capture basic identity information which may not be attached to a user,
-			/// I'm leaving it as-is for the moment.  If it's going to be any more robust, it may as well just be an LDAP,
-			/// And making an LDAP isn't the goal.
+			/// I'm leaving it as-is for the moment.  If it's going to be any more robust, it may as well just be a directory
+			/// And making a directory isn't the goal.
 			///
 			UserType newUser = ((UserFactory)Factories.getNameIdFactory(FactoryEnumType.USER)).newUser(userName,  userType, userStatus, organizationId);
 			BulkFactories.getBulkFactory().createBulkEntry(sessionId, FactoryEnumType.USER, newUser);
@@ -110,7 +128,7 @@ public class PersonService {
 			BulkFactories.getBulkFactory().createBulkEntry(sessionId, FactoryEnumType.CONTACTINFORMATION, cit);
 			newPerson.setContactInformation(cit);
 			if(email != null){
-				logger.info("Adding email to user registration: '" + email + "'");
+				logger.debug("Adding email to user registration: '" + email + "'");
 				ContactType ct = ((ContactFactory)Factories.getFactory(FactoryEnumType.CONTACT)).newContact(newUser, cDir.getId());
 				ct.setName(newPerson.getName() + " Registration Email");
 				ct.setPreferred(true);
@@ -121,29 +139,20 @@ public class PersonService {
 				cit.getContacts().add(ct);
 			}
 			else{
-				logger.warn("No email was specified during user registration");
+				logger.debug("No email was specified during user registration");
 			}
 			
 			RoleService.addUserToRole(newUser, usersUsersRole);
 			
-			BulkFactories.getBulkFactory().write(sessionId);
-			BulkFactories.getBulkFactory().close(sessionId);
-			
-			EffectiveAuthorizationService.rebuildPendingRoleCache();
-			
 			outBool = true;
 			AuditService.permitResult(audit, "Created user '" + userName + "' (#" + newUser.getId() + ")");
 		}
-		catch(ArgumentException | FactoryException | DataAccessException e) {
+		catch(ArgumentException | UnsupportedEncodingException | FactoryException | DataAccessException e) {
 			
 			AuditService.denyResult(audit, "Failed to add user: " + e.getMessage());
 			logger.error("Error creating user " + userName + ": " + e.getMessage());
 			logger.error(FactoryException.LOGICAL_EXCEPTION,e);
 
-		} catch (UnsupportedEncodingException e) {
-			
-			logger.error(e.getMessage());
-			logger.error(FactoryException.LOGICAL_EXCEPTION,e);
 		}
 		return outBool;
 	}
