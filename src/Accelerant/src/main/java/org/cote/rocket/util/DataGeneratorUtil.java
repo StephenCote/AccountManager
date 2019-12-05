a8c994c1bedba88f1669de52f6aa301a7a8d527f
Stephen Cote <sw.cote@gmail.com>
2019-12-04 16:05:56 -0800

    Debug a bulk write/update issue

--- src/Accelerant/src/main/java/org/cote/rocket/util/DataGeneratorUtil.java
+++ src/Accelerant/src/main/java/org/cote/rocket/util/DataGeneratorUtil.java
@@ -1197,6 +1197,7 @@ public class DataGeneratorUtil {
 					parentEvent.getActors().add(baby);
 					newAdditions.add(baby);
 					
+					/// Disable while checking a bug
 					addressPerson(baby,parentEvent.getLocation(), sessionId);
 					
 					EventType birth = ((EventFactory)Factories.getFactory(FactoryEnumType.EVENT)).newEvent(user, parentEvent);
