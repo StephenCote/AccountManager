66294c158b41d30eabadb5d9af95fae77143fc61
Stephen Cote <sw.cote@gmail.com>
2020-09-03 17:57:28 -0700

    Stage remote node web app project
    The node web app is meant to run scripts in a separate process via the client library, so that any aberation is contained to that node

---
+++ src/AccountManagerTaskNode/src/main/java/org/cote/rest/config/RestServiceConfig.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * Copyright (C) 2002, 2020 Stephen Cote Enterprises, LLC. All rights reserved.
+ * Redistribution without modification is permitted provided the following conditions are met:
+ *
+ *    1. Redistribution may not deviate from the original distribution,
+ *        and must reproduce the above copyright notice, this list of conditions
+ *        and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *    2. Products may be derived from this software.
+ *    3. Redistributions of any form whatsoever must retain the following acknowledgment:
+ *        "This product includes software developed by Stephen Cote Enterprises, LLC"
+ *
+ * THIS SOFTWARE IS PROVIDED BY STEPHEN COTE ENTERPRISES, LLC ``AS IS''
+ * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THIS PROJECT OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+package org.cote.rest.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.cote.accountmanager.client.ClientContext;
+import org.cote.accountmanager.client.util.AuthenticationUtil;
+import org.cote.accountmanager.client.util.CacheUtil;
+import org.cote.accountmanager.client.util.ClientUtil;
+import org.cote.accountmanager.objects.ApiClientConfigurationType;
+import org.cote.accountmanager.util.JSONUtil;
+import org.cote.accountmanager.util.StreamUtil;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+import org.glassfish.jersey.server.spi.AbstractContainerLifecycleListener;
+import org.glassfish.jersey.server.spi.Container;
+
+public class RestServiceConfig extends ResourceConfig{
+	private static final Logger logger = LogManager.getLogger(RestServiceConfig.class);
+	
+	public RestServiceConfig(@Context ServletContext servletContext){
+		register(StartupHandler.class);
+		packages("org.cote.rest.services");
+		register(RolesAllowedDynamicFeature.class);
+		
+	}
+	
+    private static class StartupHandler extends  AbstractContainerLifecycleListener {
+        @Context
+        ServletContext context;
+
+        private ClientContext receiverContext = new ClientContext();
+        private ClientContext defaultContext = new ClientContext();
+        
+        @Override
+        public void onShutdown(Container container) {
+        	if(receiverContext != null) {
+        		CacheUtil.clearCache(receiverContext);
+        		receiverContext.clearContext();
+        	}
+        	
+        }
+
+    	
+        @Override
+        public void onStartup(Container container) {
+        	initializeAccountManagerTaskNode();
+        }
+
+		private void initializeAccountManagerTaskNode(){
+			logger.info("Initializing Account Manager Task Node");
+			String cachePath = context.getInitParameter("client.cache.path");
+			String serviceName = context.getInitParameter("client.service.name");
+			String serviceUrl = context.getInitParameter("client.service.url");
+
+			ClientUtil.setCachePath(cachePath);
+			ApiClientConfigurationType api = AuthenticationUtil.getApiConfiguration(serviceUrl);
+			CacheUtil.cache(defaultContext, serviceName, api);
+
+		}
+    }
+}
---
+++ src/AccountManagerTaskNode/src/main/java/org/cote/rest/services/ScriptService.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (C) 2002, 2020 Stephen Cote Enterprises, LLC. All rights reserved.
+ * Redistribution without modification is permitted provided the following conditions are met:
+ *
+ *    1. Redistribution may not deviate from the original distribution,
+ *        and must reproduce the above copyright notice, this list of conditions
+ *        and the following disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *    2. Products may be derived from this software.
+ *    3. Redistributions of any form whatsoever must retain the following acknowledgment:
+ *        "This product includes software developed by Stephen Cote Enterprises, LLC"
+ *
+ * THIS SOFTWARE IS PROVIDED BY STEPHEN COTE ENTERPRISES, LLC ``AS IS''
+ * AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THIS PROJECT OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+package org.cote.rest.services;
+
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@DeclareRoles({"admin","user"})
+@Path("/script")
+public class ScriptService {
+	
+	private static final Logger logger = LogManager.getLogger(ScriptService.class);
+
+	@Context
+	ServletContext context;
+	
+	@Context
+	SecurityContext securityCtx;
+
+	@RolesAllowed({"admin","user"})
+	@GET
+	@Path("/{type:[A-Za-z]+}/{scriptId:[0-9A-Za-z\\-]+}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getCommunityProjectScript(@PathParam("type") String type, @PathParam("scriptId") String communityId, @Context HttpServletRequest request){
+		return Response.status(200).entity(true).build();
+	}
+	
+	
+}
