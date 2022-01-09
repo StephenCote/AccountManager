98b5aa068e2f5a44ee4ddd6b6b6750a3593b422e
Stephen Cote <sw.cote@gmail.com>
2022-01-09 13:05:48 -0600

    Update test to include group scope and distinct results

--- src/AccountManagerDataAccess/src/test/java/org/cote/accountmanager/data/TestObjectEntitlementQuery.java
+++ src/AccountManagerDataAccess/src/test/java/org/cote/accountmanager/data/TestObjectEntitlementQuery.java
@@ -18,9 +18,11 @@ import org.cote.accountmanager.objects.EntitlementType;
 import org.cote.accountmanager.objects.FieldMatch;
 import org.cote.accountmanager.objects.ObjectSearchRequestType;
 import org.cote.accountmanager.objects.SortQueryType;
+import org.cote.accountmanager.objects.UserType;
 import org.cote.accountmanager.objects.types.AuditEnumType;
 import org.cote.accountmanager.objects.types.ColumnEnumType;
 import org.cote.accountmanager.objects.types.ComparatorEnumType;
+import org.cote.accountmanager.objects.types.FactoryEnumType;
 import org.cote.accountmanager.objects.types.NameEnumType;
 import org.cote.accountmanager.objects.types.OrderEnumType;
 import org.cote.accountmanager.objects.types.QueryEnumType;
@@ -30,8 +32,8 @@ import org.junit.Test;
 
 public class TestObjectEntitlementQuery extends BaseDataAccessTest {
 
-	private void setupTestData() {
-		DirectoryGroupType dDir = BaseService.makeFind(AuditEnumType.GROUP, "DATA", "~/Data", testUser);
+	private void setupTestData(DirectoryGroupType dDir) {
+		
 		assertNotNull("Directory is null", dDir);
 		DataType data = newTextData("Search Data 1","This is the text data",testUser,dDir);
 		if(data.getDescription() == null) {
@@ -50,26 +52,112 @@ public class TestObjectEntitlementQuery extends BaseDataAccessTest {
 		}
 	}
 	
-	@Test
-	public void TestCountObjectByFieldName() {
-		setupTestData();
-		logger.info("Test search count");
-		
-
-		/// NameEnumType.DATA
-		DirectoryGroupType dDir = BaseService.makeFind(AuditEnumType.GROUP, "DATA", "~/Data", testUser);
-		DataType data3 = newTextData("Search Data 3","This is the text data",testUser,dDir);
+	private void configureAuthorization(DirectoryGroupType dDir, DataType data) {
 		BasePermissionType per1 = null;
 		BasePermissionType per2 = null;
+		try  {
+			per1 = AuthorizationService.getViewPermissionForMapType(NameEnumType.DATA, testUser.getOrganizationId());
+			per2 = AuthorizationService.getViewPermissionForMapType(NameEnumType.GROUP, testUser.getOrganizationId());
+			AuthorizationService.authorizeType(testUser, testUser2, dDir, true, false, false, false);
+			AuthorizationService.authorizeType(testUser, testUser2, data, false, false, false, false);
+			EffectiveAuthorizationService.rebuildPendingRoleCache();
+			
+		} catch (FactoryException | DataAccessException | ArgumentException e) {
+			logger.error(e.getMessage());
+			e.printStackTrace();
+		}
+		assertNotNull("Permission is null", per1);
+	}
+	/*
+	@Test
+	public void TestFilterInStructure() {
+		UserType admin = null;
+		try {
+			admin = Factories.getNameIdFactory(FactoryEnumType.USER).getByName("Admin", Factories.getDevelopmentOrganization().getId());
+		} catch (FactoryException | ArgumentException e) {
+			logger.error(e.getMessage());
+		}
+
+		DirectoryGroupType dDir = BaseService.readById(AuditEnumType.GROUP, 5826, admin);
+		assertNotNull("Dir is null", dDir);
+	
+		ObjectSearchRequestType search = new ObjectSearchRequestType();
+		search.setDistinct(true);
+		search.setObjectType(NameEnumType.PERSON);
+		SortQueryType sort = new SortQueryType();
+		sort.setSortField(QueryEnumType.NAME);
+		sort.setSortOrder(OrderEnumType.ASCENDING);
+		search.setSort(sort);
+		search.setStartRecord(0L);
+		search.setRecordCount(10);
+		search.setIncludeThumbnail(false);
+		search.setGroupScope(dDir.getObjectId());
+		FieldMatch m = new FieldMatch();
+		m.setDataType(SqlDataEnumType.TEXT);
+		m.setComparator(ComparatorEnumType.LIKE);
+		m.setEncodedValue("Test*");
+		m.setFieldName(ColumnEnumType.NAME);
+		search.getFields().add(m);
+		
+		int rawCount = BaseService.countByGroup(AuditEnumType.DATA, dDir, admin);
+		int filterCount = AuthorizedSearchService.countByEffectiveMemberEntitlement(search, admin);
+		logger.info(rawCount + ":" + filterCount);
+		
+	}
+	*/
+	
+	
+	@Test
+	public void TestFilterForOwner() {
+		DirectoryGroupType dDir = BaseService.makeFind(AuditEnumType.GROUP, "DATA", "~/Data/Test Query 1", testUser);
+		DirectoryGroupType dDir2 = BaseService.makeFind(AuditEnumType.GROUP, "DATA", "~/Data/Test Query 2", testUser);
+		setupTestData(dDir);
+		setupTestData(dDir2);
+		
+		ObjectSearchRequestType search = new ObjectSearchRequestType();
+		search.setObjectType(NameEnumType.DATA);
+		SortQueryType sort = new SortQueryType();
+		sort.setSortField(QueryEnumType.NAME);
+		sort.setSortOrder(OrderEnumType.ASCENDING);
+		search.setSort(sort);
+		search.setStartRecord(0L);
+		search.setRecordCount(10);
+		search.setIncludeThumbnail(false);
+		search.setGroupScope(dDir.getObjectId());
+		search.setDistinct(true);
+		FieldMatch m = new FieldMatch();
+		m.setDataType(SqlDataEnumType.TEXT);
+		m.setComparator(ComparatorEnumType.LIKE);
+		m.setEncodedValue("text*");
+		m.setFieldName(ColumnEnumType.MIMETYPE);
+		search.getFields().add(m);
+		
+		int rawCount = BaseService.countByGroup(AuditEnumType.DATA, dDir, testUser);
+		int filterCount = AuthorizedSearchService.countByEffectiveMemberEntitlement(search, testUser);
+		logger.info(rawCount + ":" + filterCount);
+	}
+	
+	
+	@Test
+	public void TestCountObjectByFieldName() {
+		DirectoryGroupType dDir = BaseService.makeFind(AuditEnumType.GROUP, "DATA", "~/Data/Test Query 1", testUser);
+		DirectoryGroupType dDir2 = BaseService.makeFind(AuditEnumType.GROUP, "DATA", "~/Data/Test Query 2", testUser);
+		setupTestData(dDir);
+		setupTestData(dDir2);
+
+		DataType data3 = newTextData("Search Data 3","This is the text data",testUser,dDir);
+		configureAuthorization(dDir, data3);
+		DataType data32 = newTextData("Search Data 3","This is the text data",testUser,dDir2);
+		configureAuthorization(dDir2, data32);
+		BasePermissionType per1 = null;
+		BasePermissionType per2 = null;
+		
 		try  {
 
 			per1 = AuthorizationService.getViewPermissionForMapType(NameEnumType.DATA, testUser.getOrganizationId());
 			per2 = AuthorizationService.getViewPermissionForMapType(NameEnumType.GROUP, testUser.getOrganizationId());
-			AuthorizationService.authorizeType(testUser, testUser2, dDir, true, false, false, false);
-			AuthorizationService.authorizeType(testUser, testUser2, data3, false, false, false, false);
-			EffectiveAuthorizationService.rebuildPendingRoleCache();
 			
-		} catch (FactoryException | DataAccessException | ArgumentException e) {
+		} catch (FactoryException | ArgumentException e) {
 			logger.error(e.getMessage());
 			e.printStackTrace();
 		}
@@ -80,6 +168,7 @@ public class TestObjectEntitlementQuery extends BaseDataAccessTest {
 		sort.setSortField(QueryEnumType.NAME);
 		sort.setSortOrder(OrderEnumType.ASCENDING);
 		search.setSort(sort);
+		search.setDistinct(true);
 		search.setStartRecord(0L);
 		search.setRecordCount(10);
 		search.setIncludeThumbnail(false);
@@ -98,5 +187,6 @@ public class TestObjectEntitlementQuery extends BaseDataAccessTest {
 		List<BaseGroupType> dataL = AuthorizedSearchService.searchByEffectiveMemberEntitlement(search, testUser2);
 		logger.info("Results: " + dataL.size());
 	}
+	
 	
 }
