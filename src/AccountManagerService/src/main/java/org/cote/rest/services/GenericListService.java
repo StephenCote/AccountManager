3ad198c9d412df03944427463a1d7ac6bcb838e0
Stephen Cote <sw.cote@gmail.com>
2022-02-20 14:31:47 -0600

    Add generic participant count and list interface

--- src/AccountManagerService/src/main/java/org/cote/rest/services/GenericListService.java
+++ src/AccountManagerService/src/main/java/org/cote/rest/services/GenericListService.java
@@ -31,6 +31,7 @@ import javax.annotation.security.RolesAllowed;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -42,12 +43,19 @@ import javax.ws.rs.core.UriInfo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.cote.accountmanager.data.Factories;
 import org.cote.accountmanager.data.factory.INameIdFactory;
+import org.cote.accountmanager.data.factory.IParticipationFactory;
+import org.cote.accountmanager.data.factory.ParticipationFactory;
+import org.cote.accountmanager.exceptions.ArgumentException;
 import org.cote.accountmanager.exceptions.FactoryException;
 import org.cote.accountmanager.objects.BaseGroupType;
+import org.cote.accountmanager.objects.DataTagSearchRequest;
 import org.cote.accountmanager.objects.NameIdType;
+import org.cote.accountmanager.objects.ParticipationSearchRequest;
 import org.cote.accountmanager.objects.UserType;
 import org.cote.accountmanager.objects.types.AuditEnumType;
+import org.cote.accountmanager.objects.types.FactoryEnumType;
 import org.cote.accountmanager.service.rest.BaseService;
 import org.cote.accountmanager.service.rest.SchemaBean;
 import org.cote.accountmanager.service.rest.ServiceSchemaBuilder;
@@ -78,6 +86,91 @@ public class GenericListService {
 		 schemaBean = ServiceSchemaBuilder.modelRESTService(this.getClass(),uri.getAbsolutePath().getRawPath().replaceAll("/smd$", ""));
 		 return schemaBean;
 	 }
+	
+	/*
+	 * countMembers:
+	 * 	type --- base type for the participation factory
+	 * searchRequest.participationType --- the member type [unknown - all; or specific]
+	 */
+	
+	@RolesAllowed({"user"})
+	@POST
+	@Path("/member/count")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response countMembers(@PathParam("type") String type, ParticipationSearchRequest searchRequest, @Context HttpServletRequest request){
+		AuditEnumType auditType = AuditEnumType.valueOf(type);
+
+		int count = 0;
+		if(searchRequest == null || searchRequest.getParticipations().size() == 0) {
+			logger.warn("Null or empty request");
+			return Response.status(200).entity(count).build();
+		}
+		UserType user = ServiceUtil.getUserFromSession(request);
+		try{
+			int canRead = 0;
+			for(NameIdType obj : searchRequest.getParticipations()) {
+				if(!obj.getNameType().toString().equals(type)) {
+					logger.warn("Mixed participation types not supported");
+					continue;
+				}
+				if(BaseService.canViewType(auditType, user, obj)) canRead++;
+			}
+			if(canRead != searchRequest.getParticipations().size()) {
+				logger.error("One or more provided participations is not visible to the current user");
+				return Response.status(200).entity(count).build();	
+			}
+			
+			/// get the participation factory
+			///
+			IParticipationFactory pFact = Factories.getParticipationFactory(FactoryEnumType.valueOf(type + "PARTICIPATION"));
+
+			count = pFact.countParticipations(searchRequest.getParticipations().toArray(new NameIdType[0]), searchRequest.getParticipantFactoryType());
+			
+		}
+		catch(FactoryException | ArgumentException f){
+			logger.error(f);
+		}
+		return Response.status(200).entity(count).build();
+	}
+	
+	@RolesAllowed({"user"})
+	@POST
+	@Path("/member/list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listMembers(@PathParam("type") String type, ParticipationSearchRequest searchRequest, @Context HttpServletRequest request){
+		AuditEnumType auditType = AuditEnumType.valueOf(type);
+		List<Object> objs = new ArrayList<>();
+		if(searchRequest == null || searchRequest.getParticipations().size() == 0) {
+			logger.warn("Null or empty request");
+			return Response.status(200).entity(objs).build();
+		}
+		UserType user = ServiceUtil.getUserFromSession(request);
+		try{
+			int canRead = 0;
+			for(NameIdType obj : searchRequest.getParticipations()) {
+				if(!obj.getNameType().toString().equals(type)) {
+					logger.warn("Mixed participation types not supported");
+					continue;
+				}
+				if(BaseService.canViewType(auditType, user, obj)) canRead++;
+			}
+			if(canRead != searchRequest.getParticipations().size()) {
+				logger.error("One or more provided participations is not visible to the current user");
+				return Response.status(200).entity(objs).build();	
+			}
+			
+			/// get the participation factory
+			///
+			IParticipationFactory pFact = Factories.getParticipationFactory(FactoryEnumType.valueOf(type + "PARTICIPATION"));
+			
+			objs = pFact.listParticipations(searchRequest.getParticipantFactoryType(), searchRequest.getParticipations().toArray(new NameIdType[0]), searchRequest.getStartRecord(), searchRequest.getRecordCount(), user.getOrganizationId());
+		}
+		catch(FactoryException | ArgumentException f){
+			logger.error(f);
+		}
+
+		return Response.status(200).entity(objs).build();
+	}
 	
 	@RolesAllowed({"user"})
 	@GET
