cc856d204a0e908450331a581dba44b7f3e8efbd
Stephen Cote <sw.cote@gmail.com>
2022-08-12 12:57:39 -0500

    Update request service namespace location

--- src/AccountManagerDataAccess/src/main/java/org/cote/accountmanager/data/operation/LookupAccessOperation.java
+++ src/AccountManagerDataAccess/src/main/java/org/cote/accountmanager/data/operation/LookupAccessOperation.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.cote.accountmanager.data.fact.FactUtil;
-import org.cote.accountmanager.data.security.RequestService;
+import org.cote.accountmanager.data.services.RequestService;
 import org.cote.accountmanager.objects.ApproverType;
 import org.cote.accountmanager.objects.FactEnumType;
 import org.cote.accountmanager.objects.FactType;
