44c7e19881bf44977e92eb8245608730cbbb3d2e
Stephen Cote <sw.cote@gmail.com>
2022-09-30 08:57:53 -0500

    Add context listener

---
+++ src/AccountManagerService/src/main/java/org/cote/listeners/AccountManagerContextListener.java
@@ -0,0 +1,27 @@
+package org.cote.listeners;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.cote.rest.services.WebSocketService;
+
+public class AccountManagerContextListener implements ServletContextListener{
+	public static final Logger logger = LogManager.getLogger(AccountManagerContextListener.class);
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+    	logger.info("Chirping users");
+    	WebSocketService.activeUsers().forEach(user ->{
+    		WebSocketService.chirpUser(user, new String[] {"Service going offline"});
+    	});
+		logger.info("Context destroyed");
+	}
+
+        //Run this before web application is started
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		logger.info("Context initialized");	
+	}
+}
